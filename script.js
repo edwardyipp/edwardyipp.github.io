e9c32cb113036d194ee00402d721c982512b272f
edwardyipp <edwardyipp@gmail.com>
2023-03-17 23:49:52 +0700

    update scripts

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const apiKey = '5419d873aae005a11c5146108f3ddaa0';
 
-function getWeather() {
-  const location = document.getElementById('location').value;
+// Retrieve weather information for a given city or location
+function getWeather(city, lat, lon) {
+  let url = '';
+  if (city) {
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  } else if (lat && lon) {
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  } else {
+    console.log('No location information provided.');
+    return;
+  }
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`)
+  fetch(url)
     .then(response => response.json())
     .then(data => {
       // Update city name, temperature, and weather description
@@ -20,10 +29,8 @@ function getWeather() {
       document.getElementById('wind-description').textContent = getWindDescription(data.wind.speed);
 
       // Update UV index and description
-      getUVIndex(data.coord.lat, data.coord.lon).then(index => {
-        document.getElementById('uv-index').textContent = index;
-        document.getElementById('uv-description').textContent = getUVDescription(index);
-      });
+      document.getElementById('uv-index').textContent = getUVIndex(data.coord.lat, data.coord.lon);
+      document.getElementById('uv-description').textContent = getUVDescription(getUVIndex(data.coord.lat, data.coord.lon));
 
       // Update rain value and description
       document.getElementById('rain-value').textContent = `${data.rain ? data.rain['1h'] : 0}%`;
@@ -31,8 +38,7 @@ function getWeather() {
 
       // Update background color based on city name
       const backgroundColor = getBackgroundColor(data.name);
-      document.body.classList.remove('new-york', 'london', 'paris', 'tokyo', 'sydney');
-      document.body.classList.add(backgroundColor);
+      document.body.style.backgroundColor = backgroundColor;
     })
     .catch(error => console.log(error));
 }
@@ -47,12 +53,13 @@ function getWindDescription(speed) {
   }
 }
 
-function getUVIndex(lat, lon) {
-  return fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(data => data.value)
-    .catch(error => console.log(error));
-}
+// Update UV index and description
+getUVIndex(data.coord.lat, data.coord.lon)
+  .then(uvIndex => {
+    document.getElementById('uv-index').textContent = uvIndex;
+    document.getElementById('uv-description').textContent = getUVDescription(uvIndex);
+  })
+  .catch(error => console.log(error));
 
 function getUVDescription(index) {
   if (index <= 2) {
@@ -71,16 +78,62 @@ function getUVDescription(index) {
 function getBackgroundColor(cityName) {
   switch (cityName.toLowerCase()) {
     case 'new york':
-      return 'new-york';
+      return '#f6c289';
     case 'london':
-      return 'london';
+      return '#8fc1e3';
     case 'paris':
-      return 'paris';
+      return '#e8b8d2';
     case 'tokyo':
-      return 'tokyo';
-    case 'sydney':
-      return 'sydney';
-    default:
-      return '';
-  }
-}
+      return '#a8d8ea';
+      case 'sydney':
+        return '#f5b8c8';
+        default:
+        return '#fff';
+        }
+        }
+        
+        // Ask for user's location and display weather information
+        if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        // weather information for user's current location
+        getWeather(null, latitude, longitude);
+        }, error => {
+        // Display error message if location cannot be retrieved
+        console.log(error);
+        document.getElementById('city-name').textContent = 'Error';
+        document.getElementById('weather-description').textContent = 'Cannot retrieve location information.';
+        document.getElementById('temperature').textContent = '';
+        document.getElementById('feel-like-temperature').textContent = '';
+        document.getElementById('feel-like-condition').textContent = '';
+        document.getElementById('wind-speed').textContent = '';
+        document.getElementById('wind-description').textContent = '';
+        document.getElementById('uv-index').textContent = '';
+        document.getElementById('uv-description').textContent = '';
+        document.getElementById('rain-value').textContent = '';
+        document.getElementById('rain-description').textContent = '';
+        });
+        } else {
+        // Display error message if geolocation is not supported
+        console.log('Geolocation is not supported.');
+        document.getElementById('city-name').textContent = 'Error';
+        document.getElementById('weather-description').textContent = 'Geolocation is not supported.';
+        document.getElementById('temperature').textContent = '';
+        document.getElementById('feel-like-temperature').textContent = '';
+        document.getElementById('feel-like-condition').textContent = '';
+        document.getElementById('wind-speed').textContent = '';
+        document.getElementById('wind-description').textContent = '';
+        document.getElementById('uv-index').textContent = '';
+        document.getElementById('uv-description').textContent = '';
+        document.getElementById('rain-value').textContent = '';
+        document.getElementById('rain-description').textContent = '';
+        }
+        
+        // Listen for form submission and retrieve weather information for the specified city
+        const form = document.getElementById('form');
+        form.addEventListener('submit', event => {
+        event.preventDefault();
+        const city = document.getElementById('location').value;
+        getWeather(city);
+        });
